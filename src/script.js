82be584f7e494e63924cb3cb891ad8c488e165f7
Henry <henryasante336@gmail.com>
2023-06-07 15:30:33 +0000

    More changes

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const backToTop = document.querySelector('.back2top');
+const header= document.querySelector('header');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
@@ -7,4 +8,12 @@ window.addEventListener('scroll', () => {
     else {
         backToTop.classList.remove('active');
     }
+    if(window.scrollY > 80){
+        header.classList.add('whitebg');
+    }
+    else{
+        header.classList.remove('whitebg');
+
+    }
 })
+console.log (header);
