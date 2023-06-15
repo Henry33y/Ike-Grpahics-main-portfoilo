859db9c76f9a9ea8352d8e76d75eb2fd36b8cc3c
Henry <henryasante336@gmail.com>
2023-06-15 10:33:51 +0000

    Update

--- src/script.js
+++ src/script.js
@@ -1,5 +1,11 @@
 const backToTop = document.querySelector('.back2top');
 const header= document.querySelector('header');
+const rightBtn=document.querySelector('.right-btn');
+const leftBtn=document.querySelector('.left-btn');
+const slider=document.querySelector('.myCarousel');
+const navLinks=document.querySelectorAll('.mynav-link');
+const navLinksAfter=document.querySelectorAll('.mynav-link::after');
+const reveals=document.querySelectorAll('.reveal');
 
 window.addEventListener('scroll', () => {
     if (window.scrollY > 500) {
@@ -16,4 +22,42 @@ window.addEventListener('scroll', () => {
 
     }
 })
-console.log (header);
+leftBtn.addEventListener('click',()=>{
+    console.log('left');
+    slider.style.transform='translateX(-300px)'
+    
+    
+})
+rightBtn.addEventListener('click',()=>{
+    console.log('right');
+    slider.style.transform='translateX(300px)'
+
+})
+for(let i=0;i<navLinks.length;i++){
+    navLinks[i].addEventListener('click',()=>{
+        for(let j=0;j<navLinks.length;j++){
+            if(navLinks[j].classList.contains('clicked')){
+                navLinks[j].classList.remove('clicked');
+            }
+        }
+        navLinks[i].classList.add('clicked');
+    })
+}
+const options = {
+  threshold: 0.1 // Adjust this value as needed
+};
+
+const observer = new IntersectionObserver(function(entries, observer) {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.style.animationName='reveal';
+    }
+    else{
+        entry.target.style.animationName='';
+    }
+  });
+}, options);
+
+reveals.forEach(element => {
+  observer.observe(element);
+});
